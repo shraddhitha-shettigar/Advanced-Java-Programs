736e6cd4a86ee62b09b9d4073dea2cdcc6d64e93
shraddhitha-shettigar <4al22cs151@aiet.org.in>
2025-05-20 15:02:27 +0530

    Shraddhitha

--- JSP_JDBC_Employee_Operations/Employee_Operations/database.sql
+++ JSP_JDBC_Employee_Operations/Employee_Operations/database.sql
@@ -1,13 +1,10 @@
 CREATE DATABASE IF NOT EXISTS Employee;
-
 USE Employee;
-
 CREATE TABLE IF NOT EXISTS Emp (
     Emp_NO INT PRIMARY KEY,
     Emp_Name VARCHAR(100) NOT NULL,
     Basicsalary INT
 );
-
 INSERT INTO Emp (Emp_NO, Emp_Name, Basicsalary) VALUES
 (102, 'Ravi', 10000),
 (104, 'Anil', 22000),
